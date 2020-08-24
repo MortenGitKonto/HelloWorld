4e1b4008517c0463d9d4c4d384c71fd1bb4813b7
MortenGitKonto <morten.jogi@gmail.com>
2020-08-24 12:18:31 +0300

    testisin

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+
+        public static void main(String[] args) {
+            // write your code here
+            //System.out.println(String.format("Hello World, %s!", args[0])); //1
+            System.out.println("Hello World, " + args[0] + "!");   //2
+        }
+    }
+
